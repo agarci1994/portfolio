7e914eb46730c3be59c63126ec0047e654a56e48
Alejandro García <alejandro.garci.rodr@gmail.com>
2020-07-16 11:15:02 +0200

    add download CV

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import React from 'react';
-import './App.css';
-import Main from './components/Main'
+import React from "react";
+import "./App.css";
+import Main from "./components/Main";
 
 function App() {
   return (
-    < div className = "App n-resize" >
-<Main />
+    <div className="App n-resize">
+      <div className="download">
+        <a className="link-download"
+          href="./images/CVAlejandro.pdf"
+          download="cvAlejandro.pdf"
+        >
+          Download CV
+        </a>
+      </div>
+      <Main />
     </div>
   );
 }
